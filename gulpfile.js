f5e9b28ebac346676f389ac4023a18c432ab9b1f
Artem <50479756+artemiyo@users.noreply.github.com>
2021-02-17 11:34:11 +0300

    feat: added min config for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,70 @@
-const { series } = require('gulp');
+const browsersync = require("browser-sync");
+const { src, dest, parallel, series, watch, task } = require("gulp");
+const autoprefixer = require('gulp-autoprefixer');
+const changed = require("gulp-changed");
+const clean = require("gulp-clean");
+const concat = require("gulp-concat");
+const sass = require("gulp-sass");
 
-// The `clean` function is not exported so it can be considered a private task.
-// It can still be used within the `series()` composition.
-function clean(cb) {
-  // body omitted
-  cb();
+function clear() {
+    return src(['./build/*.js', './build/*.css'], {
+        read: false,
+    })
+        .pipe(clean())
 }
 
-// The `build` function is exported so it is public and can be run with the `gulp` command.
-// It can also be used within the `series()` composition.
-function build(cb) {
-  // body omitted
-  cb();
+function html() {
+    return src('./src/*.html')
+        .pipe(dest('./build'))
+        .pipe(browsersync.stream())
 }
 
-exports.build = build;
-exports.default = series(clean, build);
+function js() {
+    const source = './src/js/*.js'
+
+    return src(source)
+        .pipe(changed(source))
+        .pipe(concat('stories.js'))
+        .pipe(dest('./build'))
+        .pipe(browsersync.stream())
+}
+
+function css() {
+    const source = './src/scss/*.scss'
+
+    return src(source)
+        .pipe(changed(source))
+        .pipe(sass())
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(concat('stories.css'))
+        .pipe(dest('./build'))
+        .pipe(browsersync.stream())
+}
+
+function watchFiles() {
+    watch('./src/*.html', html)
+    watch('./src/scss/*', css);
+    watch('./src/js/*', js);
+    watch('./src/*.html').on("change", browsersync.reload)
+}
+
+function browserSync() {
+    browsersync.init({
+        server: {
+            baseDir: './build'
+        },
+        port: 8080
+    });
+}
+
+watchFiles()
+
+task('html', html)
+task('css', css);
+task('js', js);
+task('watch', browserSync);
+task('build', series(clear, html, parallel(css, js)));
+task('dev', series('build', "watch"))
